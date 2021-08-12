014f19cc1b046b0fa251adad5490341151991c22
Dominik Huppmann <dominik.huppmann@textbroker.eu>
2021-08-12 16:25:47 +0200

    initial commit

---
+++ apps/cypress-harness-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cypress-harness-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'cypress-harness-app';
+}
