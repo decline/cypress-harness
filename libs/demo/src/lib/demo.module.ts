93f3fa505185a21400bf3339662c31e2cf481eab
Dominik Huppmann <dominik.huppmann@textbroker.eu>
2021-08-16 12:45:49 +0200

    add second component (HeaderComponent) to DemoModule

--- libs/demo/src/lib/demo.module.ts
+++ libs/demo/src/lib/demo.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DemoButtonComponent } from './demo-button/demo-button.component';
 import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [CommonModule, MatButtonModule],
   declarations: [
+    HeaderComponent,
     DemoButtonComponent
   ],
-  exports: [DemoButtonComponent]
+  exports: [HeaderComponent, DemoButtonComponent]
 })
 export class DemoModule {}
---
+++ libs/demo/src/lib/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cypress-harness-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+}
